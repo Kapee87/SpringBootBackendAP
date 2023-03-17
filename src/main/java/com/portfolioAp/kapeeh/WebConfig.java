592afe4062dee0164bee04549e8d067459614094
Kapee87 <Kapserker@gmail.com>
2023-03-16 21:24:25 -0300

    nuevo intento de deploy render 4

--- src/main/java/com/portfolioAp/kapeeh/WebConfig.java
+++ src/main/java/com/portfolioAp/kapeeh/WebConfig.java
@@ -1,30 +1,30 @@
-package com.portfolioAp.kapeeh;
-
-import org.springframework.context.annotation.Configuration;
-
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * Clase que habilita CORS
- *
- * @author Kapeeh
- */
-@EnableWebMvc
-
-@Configuration
-
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-
-    public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("/**");
-
-    }
-
-}
+//package com.portfolioAp.kapeeh;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * Clase que habilita CORS
+// *
+// * @author Kapeeh
+// */
+//@EnableWebMvc
+//
+//@Configuration
+//
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//
+//    public void addCorsMappings(CorsRegistry registry) {
+//
+//        registry.addMapping("/**");
+//
+//    }
+//
+//}
